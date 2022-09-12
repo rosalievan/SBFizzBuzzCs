4b6728286142e7eb1830910d56a5983f0fadd8bc
rosalievan <r.vanonzenoort98@gmail.com>
2022-09-12 10:45:28 +0100

    First commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zig = "blah" + "blah";
+
+            char[] charArray = zig.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
